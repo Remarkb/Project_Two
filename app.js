d509bb20a1c40f8b2865f9153ba69d1d54e8693e
hikb8 <hikb8@mail.umsl.edu>
2019-05-11 12:37:39 -0500

    all dropdowns now hide/hide depending on previous sel

--- app.js
+++ app.js
@@ -30,3 +30,47 @@ d3.csv("BCHI-dataset_2019-03-04.csv", function(hc_data) {
     data.Value = +data.Value;
   });
 });
+
+// Getting a reference to the different dropdowns
+var dd_IndCat = d3.select("#selIndCat");
+var dd_Ind = d3.select("#selInd");
+var dd_Year = d3.select("#selYear");
+var dd_Sex = d3.select("#selSex");
+var dd_RaceEth = d3.select("#selRaceEth");
+var dd_IndLoc = d3.select("#selLoc");
+
+// Unide indicator section
+dd_IndCat.on("change", function() {
+  document.getElementById("hInd").style.display = "block";
+  document.getElementById("selInd").style.display = "block";
+});
+
+// Unide Year section
+dd_Ind.on("change", function() {
+  document.getElementById("hYear").style.display = "block";
+  document.getElementById("selYear").style.display = "block";
+});
+
+// Unide Sex section
+dd_Year.on("change", function() {
+  document.getElementById("hSex").style.display = "block";
+  document.getElementById("selSex").style.display = "block";
+});
+
+// Unide Race/Eth section
+dd_Sex.on("change", function() {
+  document.getElementById("hRaceEth").style.display = "block";
+  document.getElementById("selRaceEth").style.display = "block";
+});
+
+// Unide Location section
+dd_RaceEth.on("change", function() {
+  document.getElementById("hLoc").style.display = "block";
+  document.getElementById("selLoc").style.display = "block";
+});
+
+// Unide Chart Type section
+dd_RaceEth.on("change", function() {
+  document.getElementById("hChartType").style.display = "block";
+  document.getElementById("selChartType").style.display = "block";
+});
