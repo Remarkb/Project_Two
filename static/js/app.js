2df1f87ebc0db2d420125c4a5bf829b949c1ba7b
hikb8 <hikb8@mail.umsl.edu>
2019-05-21 18:35:21 -0500

    adding Jarrett's changes to charts and updated line/scatter

--- static/js/app.js
+++ static/js/app.js
@@ -223,6 +223,14 @@ dd_Chart.on("change", function() {
     dd_PieComp.append("option").text("Race/Ethnicity");
     dd_PieComp.append("option").text("Location");
   }else if (ind_chart_text == 'Scatter') {
+    document.getElementById("hPieComp").style.display = "block";
+    document.getElementById("selPieComp").style.display = "block";
+    dd_PieComp.append("option").text("");
+    dd_PieComp.append("option").text("Indicator");
+    dd_PieComp.append("option").text("Year");
+    dd_PieComp.append("option").text("Sex");
+    dd_PieComp.append("option").text("Race/Ethnicity");
+    dd_PieComp.append("option").text("Location");
   }
 });
 
@@ -295,6 +303,8 @@ dd_PieComp.on("change", function() {
 });
 
 function create_pie_chart(x,y) {
+    var dd_cat_elm = document.getElementById("selIndCat");
+    var ind_cat_text = dd_cat_elm.options[dd_cat_elm.selectedIndex].text;
     var dd_ind_elm = document.getElementById("selInd");
     var ind_text = dd_ind_elm.options[dd_ind_elm.selectedIndex].text;
     var dd_data_elm = document.getElementById("selPieComp");
@@ -311,13 +321,15 @@ function create_pie_chart(x,y) {
      var layout = {
        size: "auto",
        showlegend: true,
-       title: ind_text + " by " + data_text
+       title: ind_cat_text
      };
      
      Plotly.newPlot("pie", data_pnt, layout);
   }
 
   function create_bar_chart(x,y) {
+    var dd_cat_elm = document.getElementById("selIndCat");
+    var ind_cat_text = dd_cat_elm.options[dd_cat_elm.selectedIndex].text;
     var dd_ind_elm = document.getElementById("selInd");
     var ind_text = dd_ind_elm.options[dd_ind_elm.selectedIndex].text;
     var dd_data_elm = document.getElementById("selPieComp");
@@ -333,55 +345,221 @@ function create_pie_chart(x,y) {
 
     var layout = {
       size: "auto",
-      // showlegend: true,
-      title: ind_text + " by " + data_text
+      showlegend: true,
+      title: ind_cat_text
     };
 
     Plotly.newPlot('pie', data, layout);
  }
 
  function create_line_chart(x,y,dp) {
+  var dd_cat_elm = document.getElementById("selIndCat");
+  var ind_cat_text = dd_cat_elm.options[dd_cat_elm.selectedIndex].text;
   var dd_ind_elm = document.getElementById("selInd");
   var ind_text = dd_ind_elm.options[dd_ind_elm.selectedIndex].text;
   var dd_data_elm = document.getElementById("selPieComp");
   var data_text = dd_data_elm.options[dd_data_elm.selectedIndex].text;
 
-  var trace1 = {
-    x: y,
-    y: x,
-    type: 'scatter'
-  };
+  var arrayLength = dp.length;
+  var counts = {};
+  var counter = 0;
+  var amtPerCont = 0;
+  for (var i = 0; i < arrayLength; i++) {
+      counts[dp[i]] = 1 + (counts[dp[i]] || 0);
+  }
+  counter = Object.keys(counts).length;
+  amtPerCounter = dp.length / counter;
+  console.log(dp);
+  if (counter >= 1) {
+    var trace1 = {
+      x: y.slice(0,amtPerCounter),
+      y: x.slice(0,amtPerCounter),
+      type: 'scatter',
+      name: dp[0]
+    };
+  }
+  if (counter >= 2) {
+    var trace2 = {
+      x: y.slice(amtPerCounter,amtPerCounter*2),
+      y: x.slice(amtPerCounter,amtPerCounter*2),
+      type: 'scatter',
+      name: dp[amtPerCounter]
+    };
+  }
+  if (counter >= 3) {
+    var trace3 = {
+      x: y.slice(amtPerCounter*2,amtPerCounter*3),
+      y: x.slice(amtPerCounter*2,amtPerCounter*3),
+      type: 'scatter',
+      name: dp[amtPerCounter*2]
+    };
+  }
+  if (counter >= 4) {
+    var trace4 = {
+      x: y.slice(amtPerCounter*3,amtPerCounter*4),
+      y: x.slice(amtPerCounter*3,amtPerCounter*4),
+      type: 'scatter',
+      name: dp[amtPerCounter*3]
+    };
+  }
+  if (counter == 5) {
+    var trace5 = {
+      x: y.slice(amtPerCounter*4,amtPerCounter*5),
+      y: x.slice(amtPerCounter*4,amtPerCounter*5),
+      type: 'scatter',
+      name: dp[amtPerCounter*4]
+    };
+  }
+
+  if (counter == 1) {
+    var data = [trace1];
+  } else if (counter == 2) {
+    var data = [trace1, trace2];
+  } else if (counter == 3) {
+    var data = [trace1, trace2, trace3];
+  } else if (counter == 4) {
+    var data = [trace1, trace2, trace3, trace4];
+  } else if (counter == 5) {
+    var data = [trace1, trace2, trace3, trace4, trace5];
+  }
 
   var layout = {
     size: "auto",
     showlegend: true,
-    title: ind_text + " by " + data_text
+    title: ind_cat_text,
+    xaxis: {
+      title: 'Year'
+    },
+    yaxis: {
+      title: ind_text
+    }
   };
-  
-  var data = [trace1];
-  
+
   Plotly.newPlot('pie', data, layout);
 }
 
 function create_scatter_chart(x,y,dp) {
+  var dd_cat_elm = document.getElementById("selIndCat");
+  var ind_cat_text = dd_cat_elm.options[dd_cat_elm.selectedIndex].text;
   var dd_ind_elm = document.getElementById("selInd");
   var ind_text = dd_ind_elm.options[dd_ind_elm.selectedIndex].text;
+  var dd_year_elm = document.getElementById("selYear");
+  var year_text = dd_year_elm.options[dd_year_elm.selectedIndex].text;
+  var dd_sex_elm = document.getElementById("selSex");
+  var sex_text = dd_sex_elm.options[dd_sex_elm.selectedIndex].text;
+  var dd_race_elm = document.getElementById("selRaceEth");
+  var race_text = dd_race_elm.options[dd_race_elm.selectedIndex].text;
+  var dd_loc_elm = document.getElementById("selLoc");
+  var loc_text = dd_loc_elm.options[dd_loc_elm.selectedIndex].text;
+  var dd_chart_elm = document.getElementById("selChartType");
+  var ind_chart_text = dd_chart_elm.options[dd_chart_elm.selectedIndex].text;  
   var dd_data_elm = document.getElementById("selPieComp");
-  var data_text = dd_data_elm.options[dd_data_elm.selectedIndex].text;
+  var data_text = dd_data_elm.options[dd_data_elm.selectedIndex].text;  
 
-  var trace1 = {
-    x: x,
-    y: dp,
-    type: 'scatter',
-  };
-  
-  var data_pnt = [trace1];
-  
+  if (ind_text == 'All'){
+    ind_text = "Value/Percent"
+  }
+
+  var arrayLength = dp.length;
+  var counts = {};
+  var counter = 0;
+  var amtPerCont = 0;
+  for (var i = 0; i < arrayLength; i++) {
+      counts[dp[i]] = 1 + (counts[dp[i]] || 0);
+  }
+  counter = Object.keys(counts).length;
+  amtPerCounter = dp.length / counter;
+  if (counter > 5) {
+    if(ind_text == 'All') {
+      ind_text = '%%';}
+    if(year_text == 'All') {
+      year_text = '%%';}
+    if(sex_text == 'All') {
+      sex_text = '%%';}
+    if(race_text == 'All') {
+      race_text = '%%';}  
+    if(loc_text == 'All') {
+      loc_text = '%%';}
+    alert("A line chart is not the best type of chart for your data selection.  Please try a bar or pie chart instead.");
+    d3.json(`/sel_pie/${ind_cat_text}/${ind_text}/${year_text}/${sex_text}/${race_text}/${loc_text}/${data_text}`).then((data) => {
+      var un_zip = _.unzip(data);
+      var y = un_zip[0];
+      var string_num = un_zip[1];
+      console.log(string_num)
+      var x = string_num.map(v => +v );
+      create_pie_chart(x,y);
+    });
+    return;
+  }
+  if (counter >= 1) {
+    var trace1 = {
+      x: y.slice(0,amtPerCounter),
+      y: x.slice(0,amtPerCounter),
+      mode: 'markers',
+      type: 'scatter',
+      name: dp[0]
+    };
+  }
+  if (counter >= 2) {
+    var trace2 = {
+      x: y.slice(amtPerCounter,amtPerCounter*2),
+      y: x.slice(amtPerCounter,amtPerCounter*2),
+      mode: 'markers',
+      type: 'scatter',
+      name: dp[amtPerCounter]
+    };
+  }
+  if (counter >= 3) {
+    var trace3 = {
+      x: y.slice(amtPerCounter*2,amtPerCounter*3),
+      y: x.slice(amtPerCounter*2,amtPerCounter*3),
+      mode: 'markers',
+      type: 'scatter',
+      name: dp[amtPerCounter*2]
+    };
+  }
+  if (counter >= 4) {
+    var trace4 = {
+      x: y.slice(amtPerCounter*3,amtPerCounter*4),
+      y: x.slice(amtPerCounter*3,amtPerCounter*4),
+      mode: 'markers',
+      type: 'scatter',
+      name: dp[amtPerCounter*3]
+    };
+  }
+  if (counter == 5) {
+    var trace5 = {
+      x: y.slice(amtPerCounter*4,amtPerCounter*5),
+      y: x.slice(amtPerCounter*4,amtPerCounter*5),
+      mode: 'markers',
+      type: 'scatter',
+      name: dp[amtPerCounter*4]
+    };
+  }
+
+  if (counter == 1) {
+    var data = [trace1];
+  } else if (counter == 2) {
+    var data = [trace1, trace2];
+  } else if (counter == 3) {
+    var data = [trace1, trace2, trace3];
+  } else if (counter == 4) {
+    var data = [trace1, trace2, trace3, trace4];
+  } else if (counter == 5) {
+    var data = [trace1, trace2, trace3, trace4, trace5];
+  }
+
   var layout = {
     size: "auto",
     showlegend: true,
-    title: ind_text + " by " + data_text
+    title: ind_cat_text,
+    xaxis: {
+      title: 'Year'
+    },
+    yaxis: {
+      title: ind_text
+    }
   };
-  
-  Plotly.newPlot("pie", data_pnt, layout);
+
+  Plotly.newPlot('pie', data, layout);
 }
